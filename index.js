ca26e64997208b989ebface636f82d0d123e05dd
Mikhail Zheltovskikh <misha14051994@mail.ru>
2025-04-24 20:33:37 +0500

    инифиализация проекта

---
+++ index.js
@@ -0,0 +1,34 @@
+function generateRandomPassword(options = {}) {
+    const {
+      length = 12,
+      includeNumbers = true,
+      includeSymbols = true,
+      includeUppercase = true,
+      includeLowercase = true
+    } = options;
+  
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
+    let chars = '';
+    if (includeLowercase) chars += lowercase;
+    if (includeUppercase) chars += uppercase;
+    if (includeNumbers) chars += numbers;
+    if (includeSymbols) chars += symbols;
+  
+    if (!chars) throw new Error('At least one character type must be enabled');
+  
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      password += chars[randomIndex];
+    }
+  
+    return password;
+  }
+  
+  
+  console.log(generateRandomPassword());
+  console.log(generateRandomPassword({ length: 16, includeSymbols: false }));
